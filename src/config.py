498f60a8bbd3dd09ad5f56ff5a9ffc60491b2306
James Morgan <james.morgan@servian.com>
2022-06-09 21:05:05 +1000

    add GCS_ env variables

--- src/config.py
+++ src/config.py
@@ -5,3 +5,6 @@ config = dotenv_values("src/.env")
 INVERTER_ID = config["GOODWE_STATION_ID"]
 INVERTER_USER = config["GOODWE_USER"]
 INVERTER_PASS = config["GOODWE_PASSWORD"]
+
+GCS_BUCKET = config["GCS_BUCKET_NAME"]
+GCS_KEY_PATH = config["GCS_SERVICE_ACCOUNT_KEY"]
