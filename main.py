dea4425591311ab826488d1b56d92f3ddf92d60d
James Morgan <james.morgan@servian.com>
2022-06-09 23:33:04 +1000

    update data processing

--- main.py
+++ main.py
@@ -28,9 +28,9 @@ def get_current_solar_data():
     inv = SingleInverter(INVERTER_ID, INVERTER_USER, INVERTER_PASS)
 
     df = pd.DataFrame({
-        'timestamp': inv.data['inverter']['time'],
-        'current_output_kw': inv.data['inverter']['output_power'] / 1000,
-        'total_production_today': inv.data['inverter']['eday']
+        'timestamp': [inv.data['inverter']['time']],
+        'current_output_kw': [inv.data['inverter']['output_power']],
+        'total_production_today': [inv.data['inverter']['eday']]
     })
 
     dataframe_to_storage_blob_as_csv(GCS_BUCKET, df, f"{date.strftime('%Y%m%d')}/inverter_log_{inv.data['inverter']['time']}")
