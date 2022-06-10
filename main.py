9da5c852157c02f275dbc09879da68df88ce2d27
James Morgan <james.morgan@servian.com>
2022-06-10 11:34:39 +1000

    fix remove forward slashes from timestamp to prevent excessive folder creation in blob storage

--- main.py
+++ main.py
@@ -27,14 +27,17 @@ def dataframe_to_storage_blob_as_csv(bucket_name, dataframe, destination_blob_fi
 def get_current_solar_data():
     inv = SingleInverter(INVERTER_ID, INVERTER_USER, INVERTER_PASS)
 
-    timestamp = inv.data['inverter']['time'].replace('/', '')   # default format includes "/" which messes with folder creation in blob storage
     df = pd.DataFrame({
-        'timestamp': [timestamp],
+        'timestamp': [inv.data['inverter']['time']],
         'current_output_kw': [inv.data['inverter']['output_power']],
         'total_production_today': [inv.data['inverter']['eday']]
     })
 
-    dataframe_to_storage_blob_as_csv(GCS_BUCKET, df, f"{date.today().strftime('%Y%m%d')}/inverter_log_{inv.data['inverter']['time']}")
+    dataframe_to_storage_blob_as_csv(
+        GCS_BUCKET,
+        df,
+        f"{date.today().strftime('%Y%m%d')}/inverter_log_{inv.data['inverter']['time'].replace('/', '')}  # default format includes "/" which messes with folder creation in blob storage}"
+    )
 
 
 if __name__ == "__main__":
