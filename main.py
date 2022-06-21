6dea7577c76db1148af7a4c9da9dfaf5a907d18f
James Morgan <james.morgan@servian.com>
2022-06-21 10:55:15 +1000

    Update data format from csv to json (entire inverter record)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
 from datetime import datetime
 
-import pandas as pd
 from google.cloud import storage
 from pygoodwe import SingleInverter
 from pytz import timezone
@@ -8,7 +8,7 @@ from pytz import timezone
 from src.config import INVERTER_ID, INVERTER_USER, INVERTER_PASS, GCS_BUCKET
 
 
-def dataframe_to_storage_blob_as_csv(bucket_name, dataframe, destination_blob_filename):
+def to_storage_bucket(bucket_name, data, destination_blob_filename):
     """Uploads a file to the bucket."""
     # The ID of your GCS bucket
     # bucket_name = "your-bucket-name"
@@ -19,7 +19,7 @@ def dataframe_to_storage_blob_as_csv(bucket_name, dataframe, destination_blob_fi
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(destination_blob_filename)
 
-    blob.upload_from_string(dataframe.to_csv(), 'text/csv')
+    blob.upload_from_string(data, 'application/json')
 
     print(
         f"Data uploaded to {destination_blob_filename}."
@@ -29,18 +29,15 @@ def dataframe_to_storage_blob_as_csv(bucket_name, dataframe, destination_blob_fi
 def get_current_solar_data(event, context):
     inv = SingleInverter(INVERTER_ID, INVERTER_USER, INVERTER_PASS)
 
-    df = pd.DataFrame({
-        'timestamp': [inv.data['inverter']['time']],
-        'current_output_kw': [inv.data['inverter']['output_power']],
-        'total_production_today': [inv.data['inverter']['eday']]
-    })
+    # dump single inverter record to string (one line, ok for BigQuery)
+    data = json.dumps(inv.data['inverter'])
 
     timestamp = datetime.strptime(inv.data['inverter']['time'], "%m/%d/%Y %H:%M:%S")
     timestamp = datetime.strftime(timestamp, "%Y%m%d %H:%M")
 
-    dataframe_to_storage_blob_as_csv(
+    to_storage_bucket(
         GCS_BUCKET,
-        df,
+        data,
         f"{datetime.now(timezone('Australia/Melbourne')).strftime('%Y%m%d')}/inverter_log_{timestamp}"  # default format includes '/' which messes with folder creation in blob storage}"
     )
 
